72927fc1a74cc146e78d3934e5e3421834314664
jacobrsv <j@cobs.dk>
2025-04-17 13:24:17 +0200

    Update .gitignore and scripts for consistency; fix prefix in serienummer.ps1 and add sysprepped image

--- scripts/serienummer.ps1
+++ scripts/serienummer.ps1
@@ -1,3 +1,3 @@
 # Finder computerens serienummer og tilføjer en prefix
 # f.eks virksomhedsnavn-PF1S604Y
-return "CONTOSE-$(Get-CimInstance -ClassName Win32_BIOS | Select-Object -ExpandProperty SerialNumber)"
+return "CONTOSO-$(Get-CimInstance -ClassName Win32_BIOS | Select-Object -ExpandProperty SerialNumber)"
