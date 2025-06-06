76563fc0c210a59a0c907b68f5ba8af53428e524
jacobrsv <j@cobs.dk>
2025-06-06 12:12:15 +0200

    Bedre logs fra caddy

--- start_server.sh
+++ start_server.sh
@@ -5,7 +5,7 @@
 ######                      Jacob Rusch Svendsen                         ######
 ###############################################################################
 journalctl _EXE=/usr/bin/dnsmasq -f &
-caddy run  &
+caddy run | jq -r '"\(.msg) \(.request.method) \(.request.uri) \(.request.remote_ip)"' &
 cd app
 .venv/bin/python app.py &
 chromium --incognito "http://osdeploy:8000" > /dev/null 2>&1 & 
